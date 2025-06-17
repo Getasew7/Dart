6b08f985fbad0acd3f00c864a405386d3311adf8
Getasew7 <getasewendaylalu7721@gmail.com>
2025-06-17 12:28:43 +0300

    control flow statement

---
+++ control_flow_statement.dart
@@ -0,0 +1,12 @@
+if (year < 2020) {
+  print('The year is before 2020.');
+} else if (year == 2020) {
+  print('The year is 2020.');
+} else {
+  print('The year is after 2020.');
+}
+else if (year > 2020) {
+  print('The year is after 2020.');
+} else {
+  print('Invalid year.');
+}
